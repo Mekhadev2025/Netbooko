2f64dde1515fd3b7905645da597abca6ae0997b5
Mekhadev2025 <mekhalnair@gmail.com>
2023-03-25 19:17:44 +0530

    4

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,24 @@
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
+import { Navbar } from "./components/Navbar"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
 
-
-import {Route,Routes} from "react-router-dom"
-import {Container} from "react-bootstrap"
-import {Home} from "./pages/Home"
-import {Store} from "./pages/Store"
-import {About} from "./pages/About"
-import {Navbar}  from "./components/Navbar"
-
-import './App.css'
- 
 function App() {
- 
-
   return (
-    <>
-    <Navbar></Navbar>
-    <Container className="mb-4">
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className="mb-4">
         <Routes>
-        <Route path="/" element ={<Home/>}></Route>
-        <Route path="/store" element ={<Store/>}></Route>
-        <Route path="/about" element ={<About/>}></Route>
-     </Routes>
-    </Container>
-
-
-
-    </>
-    
-    )
-  
-
-
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
+  )
 }
 
 export default App
